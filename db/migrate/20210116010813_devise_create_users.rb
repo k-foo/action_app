2be9f4d3493c9904a09fec68ecccfd7ce40fe2f0
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-16 16:29:10 +0900

    マイグレーションファイルの作成。
    ニックネームと生年月日を追加。

--- db/migrate/20210116010813_devise_create_users.rb
+++ db/migrate/20210116010813_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, unique: true
+      t.date   :birthdate,          null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
