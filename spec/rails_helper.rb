d9244aeafec3e23b9b7d44dd3efff1b5a43ac174
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-18 08:08:59 +0900

    objectives／結合テストコードの作成。
    目標設定において、投稿に関する一連の流れができるかテストで確認できるようにするため。ログインを前提とする結合テストは今後も作成するため、「ログイン部分」について別のモジュールを作成した。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,8 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# 「ログインを前提とする結合コード」にて使用する「ログイン部分」のモジュールを作成したいためコメントアウトを外す
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,6 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # 「ログインを前提とする結合コード」にて使用する「ログイン部分」のモジュールを作成したいため一文追加する
+  config.include LogInSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
