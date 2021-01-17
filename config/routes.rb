e05769516c295c5872f3e136821b65bc6095b156
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-17 20:02:16 +0900

    objectives／ルーティングの設定。
    これから７つのアクションについて実装するため。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'objectives/index'
-  get 'objectives/new'
-  get 'objectives/create'
   devise_for :users
   root to: 'homes#index'
   get 'homes/index'
+  resources :objectives
 end
