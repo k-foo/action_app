e99fa82e2fbea6a15134cb5a74b669609a037fb7
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-23 19:26:00 +0900

    specに関するファイル生成・削除。
    コントローラー作成時にできたもの。

--- spec/models/trouble_spec.rb
+++ spec/models/trouble_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "worries/create.html.erb", type: :view do
+RSpec.describe Trouble, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
