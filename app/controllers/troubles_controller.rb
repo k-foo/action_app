e54351c7492c9c7cfd0e81a311ac619dc518e476
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-23 19:21:41 +0900

    troubles／コントローラーの設定。

---
+++ app/controllers/troubles_controller.rb
@@ -0,0 +1,22 @@
+class TroublesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @troubles = current_user.troubles.all.order(created_at: :desc)
+    @trouble = Trouble.new
+  end
+
+  def new
+  end
+
+  def create
+    trouble = Trouble.new(trouble_params)
+    render json: { trouble: trouble } if trouble.save
+  end
+
+  private
+
+  def trouble_params
+    params.require(:trouble).permit(:worry).merge(user_id: current_user.id)
+  end
+end
