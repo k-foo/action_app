dbf24a6b6802e44cf4c7ee869efa8d24fba08757
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-24 11:37:09 +0900

    悩みが非同期通信で投稿・保存できる実装。
    フォームに投稿するたびに、メッセージが一覧にレンダリングされるように実装。

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,30 +1,36 @@
 function post() {
-  const submit = document.getElementById("submit_btn");
-  submit.addEventListener("click", (e) => {
-    const formData = new FormData(document.getElementById("new_article"));
+    const submit = document.getElementById("submit_worry");
+    submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form_worry"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/troubles", true);
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      if (XHR.status != 200 ){
+      if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       }
       const item = XHR.response.trouble;
-      //レスポンスのうち、コントローラー側で指定したjson形式のデータを変数に代入
-      const contentsArea = document.getElementById("contents_area");
-      //今回投稿したデータを追加する要素の取得(追加する要素の親要素)
+      const list = document.getElementById("list_messages");
+      const formText = document.getElementById("content_worry");
       const HTML = `
-       <div class="trouble">
-          ${ item.trouble }
-        </div>`;
-      //親要素に直前で定義した要素を追加
-      contentsArea.insertAdjacentHTML("afterbegin", HTML);
-      //親要素内の最上部に追加
-      articleText.value = "";
+      <li class="message-left-side">
+        <div class="pic-chat">
+          <img src="/assets/sad.png" alt="落ち込んでいる画像">
+        </div>
+        <div class="text-chat">
+        <div class="post-content">
+          ${item.worry}
+        </div>
+        <div class="post-date">
+          投稿日時：${item.created_at}
+        </div>
+      </li>`;
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = "";
     };
     e.preventDefault();
-  })
+  });
 }
 window.addEventListener('load', post);
