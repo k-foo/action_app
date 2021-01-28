2d75f803fa299c3a9281cc9ab6dfe93316c787cb
erika618 <68714247+erika618@users.noreply.github.com>
2021-01-28 10:02:39 +0900

    「励まし」部分のJavaScript削除。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../passcheck")
 require("../post")
-require("../courage_post")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
