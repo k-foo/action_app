ab3d18bff80f8b0c4e79666437666040a5dfb862
erika618 <68714247+erika618@users.noreply.github.com>
2021-02-13 13:22:42 +0900

    マイグレーションファイルの設定。

--- db/migrate/20210213034354_create_rules.rb
+++ db/migrate/20210213034354_create_rules.rb
@@ -1,7 +1,17 @@
 class CreateRules < ActiveRecord::Migration[6.0]
   def change
     create_table :rules do |t|
-
+      t.references :user,null: false, foreign_key: true
+      t.string :if_1    
+      t.string :then_1  
+      t.string :if_2    
+      t.string :then_2  
+      t.string :if_3    
+      t.string :then_3  
+      t.string :if_4    
+      t.string :then_4  
+      t.string :if_5    
+      t.string :then_5   
       t.timestamps
     end
   end
