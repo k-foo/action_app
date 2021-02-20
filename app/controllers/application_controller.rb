bdb39a3d60eff0a1200da5102ba54630bea3da74
erika618 <68714247+erika618@users.noreply.github.com>
2021-02-20 15:06:15 +0900

    不要なメソッドを削除。
    rails_best_practices .により不要であることがわかったため。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # ログアウト後はルートパスとする（この設定をしないと新規登録者に対してログインを求めてしまうため）
-  def after_sign_out_path_for(_resource_or_scope)
-    root_path
-  end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
